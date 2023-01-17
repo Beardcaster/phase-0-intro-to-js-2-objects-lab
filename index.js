87d112dde52475a6fad5531669e78b76e57aab1e
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-16 17:04:27 -0700

    commit tree 44976e81d58976f55742069b3287b5cf848dbfd0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    "name": "Jayson",
+    "streetAddress": "2526 Courtland"};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj, [key]: value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  
+    const newObj = {...obj};
+    delete newObj["name"]; //bracket notation
+    console.log(newObj);
+
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    obj = delete obj["name"];
+    return employee;
+}
+
+
